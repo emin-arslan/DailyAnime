24e2bf2c651036548c7b931df24d872aac953bc8
emin-arslan <eminarslan1864@gmail.com>
2023-07-29 14:01:25 +0300

    Update Navi.js

--- frontend/src/components/navi/Navi.js
+++ frontend/src/components/navi/Navi.js
@@ -3,8 +3,8 @@ import React from "react";
 const Navi = () => {
   return (
     <div className="w-full drop-shadow-md bg-white sticky top-0 z-30">
-      <div className="container mx-auto ">
-        <div className="py-4">
+      <div className="container mx-auto min-w-fit ">
+        <div className="py-4 ">
           <span className="text-2xl drop-shadow-md  text-blue-500 font-semibold font-poppins">
             DailyAnime
           </span>
