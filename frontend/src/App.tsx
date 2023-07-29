c532a0d7817a473705f7ee3546c1139a19c7fd7a
emin-arslan <eminarslan1864@gmail.com>
2023-07-29 13:58:58 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,10 +23,13 @@ function App() {
       <div className="container mx-auto flex justify-center min-w-[300px] sm:w-[600px] ">
       {
         animeCards.animeDataReducer.cards.length < 2 ? 
-        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <div className="flex w-full h-[60vh] justify-center items-center">
+          <svg className="animate-spin -ml-1 mr-3 h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
       <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
       <path className="opacity-75" fill="black" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
     </svg>
+          
+           </div>
     :  <div className="grid grid-cols-6 w-fit gap-4 mt-5 md:grid-cols-4 sm:grid-cols-2 sm:w-[500px] ">
     {
       animeCards.animeDataReducer.cards.map(card => {
