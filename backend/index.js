5742e7683589e584064998a5604b116b8e38f76b
emin-arslan <eminarslan1864@gmail.com>
2023-07-29 12:07:36 +0300

    refactor index.js

--- backend/index.js
+++ backend/index.js
@@ -30,31 +30,34 @@ console.log("buraya girdi.")
 
 */
 app.get("/anime-cards", async (req, resp) => {
-  console.log("buraya girdi.")
-  let trAnimeCards = []
   try {
-    const animeData = await TrAnime();
-    trAnimeCards = [...animeData]
-  } catch (error) {
-    console.error("Hata:", error);
-  }
-  console.log(trAnimeCards)
-  console.log("#".repeat(100))
-  const chineseAnimeCards = await ChineseAnime()
-  console.log( chineseAnimeCards );
-  const resultArray = [];
-  for (let index = 0; index < 9; index++) {
-    resultArray.push(chineseAnimeCards[index]);
-    resultArray.push(trAnimeCards[index])
-  }
-  await Anime.deleteMany({});
-  await Anime.insertMany(resultArray);
+    console.log("buraya girdi.");
+    let trAnimeCards = [];
 
-  resp.status(200).json({ body: await Anime.find({}) });
+    const animeData = await TrAnime();
+    trAnimeCards = [...animeData];
+
+    console.log(trAnimeCards);
+    console.log("#".repeat(100));
+    const chineseAnimeCards = await ChineseAnime();
+    console.log(chineseAnimeCards);
+    const resultArray = [];
+    for (let index = 0; index < 9; index++) {
+      resultArray.push(chineseAnimeCards[index]);
+      resultArray.push(trAnimeCards[index]);
+    }
+    await Anime.deleteMany({});
+    await Anime.insertMany(resultArray);
+
+    resp.status(200).json({ body: await Anime.find({}) });
+  } catch (error) {
+    console.error("handle error", error);
+    resp.status(500).json({body:"getfailed"});
+  }
 });
 
-app.get("/",(req,resp)=>{
-  resp.status(200).json({status:"OK"})
-})
+app.get("/", (req, resp) => {
+  resp.status(200).json({ status: "OK" });
+});
 
 app.listen(5000);
