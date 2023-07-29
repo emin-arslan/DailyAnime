d286b17a90ebbf032c7bcd30c25eb6207a83bbe2
emin-arslan <eminarslan1864@gmail.com>
2023-07-29 13:53:46 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,33 +21,40 @@ function App() {
       <Player modal={modal} video={video} setModal={setModal}/>
       <Navi />
       <div className="container mx-auto flex justify-center min-w-[300px] sm:w-[600px] ">
-
-
-        <div className="grid grid-cols-6 w-fit gap-4 mt-5 md:grid-cols-4 sm:grid-cols-2 sm:w-[500px] ">
-          {
-            animeCards.animeDataReducer.cards.map(card => {
-              const color = card.source == "Chinese" ? "from-green-900":"from-red-900"
-              return (
-                <div>
-                  <div  className="relative hover:cursor-pointer hover:z-20 hover:scale-[1.17] transition-all s ">
-                    <img onClick={() => {
-                    setVideo(card.videoUrl)
-                    setModal(true)
-                  }} alt="resim" src={card.imageUrl} className="w-40 h-[225px]  rounded-xl"></img>
-                    <div className="absolute bottom-0 text-sm font-semibold px-2 py-2 w-full text-gray-100 bg-gradient-to-t  from-black">
-                      {card.episode}
-                    </div>
+      {
+        animeCards.animeDataReducer.cards.length < 1 ? 
+        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path className="opacity-75" fill="black" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+    :  <div className="grid grid-cols-6 w-fit gap-4 mt-5 md:grid-cols-4 sm:grid-cols-2 sm:w-[500px] ">
+    {
+      animeCards.animeDataReducer.cards.map(card => {
+        const color = card.source == "Chinese" ? "from-green-900":"from-red-900"
+        return (
+          <div>
+            <div  className="relative hover:cursor-pointer hover:z-20 hover:scale-[1.17] transition-all s ">
+              <img onClick={() => {
+              setVideo(card.videoUrl)
+              setModal(true)
+            }} alt="resim" src={card.imageUrl} className="w-40 h-[225px]  rounded-xl"></img>
+              <div className="absolute bottom-0 text-sm font-semibold px-2 py-2 w-full text-gray-100 bg-gradient-to-t  from-black">
+                {card.episode}
+              </div>
+        
+              <div
+               onClick={()=>window.open(card.watchLink,"_blank")} className={`absolute top-0 right-0 p-1 px-2 bg-gradient-to-b  z-20 text-xs text-white ${color} hover:animate-pulse font-semibold rounded-tr-xl`}>{card.source}</div>
               
-                    <div
-                     onClick={()=>window.open(card.watchLink,"_blank")} className={`absolute top-0 right-0 p-1 px-2 bg-gradient-to-b  z-20 text-xs text-white ${color} hover:animate-pulse font-semibold rounded-tr-xl`}>{card.source}</div>
-                    
-                  </div>
-                  <p className="w-40 text-xs text-center line-clamp-2">{card.title}</p>
-                </div>
-              )
-            })
-          }
-        </div>
+            </div>
+            <p className="w-40 text-xs text-center line-clamp-2">{card.title}</p>
+          </div>
+        )
+      })
+    }
+  </div>
+      }
+
+       
       </div>
     </div>
   );
