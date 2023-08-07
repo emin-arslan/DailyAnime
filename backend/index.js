a7d49b295965c34a2e8d7579bcb7fbdc41d5f0a0
emin-arslan <eminarslan1864@gmail.com>
2023-08-07 22:52:47 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ const cors = require("cors");
 const cronJob = require("./cronJob");
 
 const app = express();
-cronJob();
 app.use(cors());
 app.use(express.json());
+cronJob();
 
 app.get("/animeCards", async (req, resp) => {
   resp.status(200).json({ body: await Anime.find({}) });
