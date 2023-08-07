99777865e83eaefef4b282648336ddae8361dbe6
emin-arslan <eminarslan1864@gmail.com>
2023-08-08 01:23:06 +0300

    Update config.js

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
-const connectDb = mongoose.connect("mongodb+srv://deletedark:del123ete@earthquake-project.hpymb.mongodb.net/");
+const connectDb = mongoose.connect("mongodb+srv://deletedark:del123ete@earthquake-project.hpymb.mongodb.net/animeDB");
