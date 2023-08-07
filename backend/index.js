bacdf712e12f4e0e7781179cffe151888f1f257e
emin-arslan <eminarslan1864@gmail.com>
2023-08-08 00:16:37 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,29 +8,31 @@ app.use(cors());
 app.use(express.json());
 
 async function setAnimeDatas () {
-  console.log("babahere")
-  let turkAnimeCards = [];
-  const resultCardArray = [];
-  const chineseAnimeCards = await AnimeXin();
   try {
-    turkAnimeCards = await TurkAnime();
+    console.log("babahere")
+    let turkAnimeCards = [];
+    const resultCardArray = [];
+    const chineseAnimeCards = await AnimeXin();
+    try {
+      turkAnimeCards = await TurkAnime();
+    } catch (error) {
+      console.error("Hata oluştu:", error);
+    }
+    for (let index = 0; index < 9; index++) {
+      resultCardArray.push(chineseAnimeCards[index]);
+      resultCardArray.push(turkAnimeCards[index]);
+    }
+    await Anime.deleteMany({});
+    await Anime.insertMany(resultCardArray);
   } catch (error) {
-    console.error("Hata oluştu:", error);
+    console.error("setAnimeDatas Hata:", error);
+  } finally {
+    // setAnimeDatas fonksiyonu tamamlandıktan sonra tekrar çağır
+    setTimeout(setAnimeDatas, 200000); // Her 5 dakikada bir (300000 ms) setAnimeDatas fonksiyonunu çağırır
   }
-  for (let index = 0; index < 9; index++) {
-    resultCardArray.push(chineseAnimeCards[index]);
-    resultCardArray.push(turkAnimeCards[index]);
-  }
-  await Anime.deleteMany({});
-  await Anime.insertMany(resultCardArray);
 }
 
-async function executeSetAnimeDatas() {
-  await setAnimeDatas();
-  console.log("setAnimeDatas tamamlandı.");
-}
-
-setInterval(executeSetAnimeDatas, 300000);
+setAnimeDatas(); // İlk kez başlatmak için setAnimeDatas fonksiyonunu çağır
 
 app.get("/animeCards", async (req, resp) => {
   resp.status(200).json({ body: await Anime.find({}) });
