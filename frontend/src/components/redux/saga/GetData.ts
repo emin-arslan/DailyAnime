eb28edbe5f836323dc2a60e9e8183d7ca89e7541
emin-arslan <eminarslan1864@gmail.com>
2023-08-07 22:55:28 +0300

    Update GetData.ts

--- frontend/src/components/redux/saga/GetData.ts
+++ frontend/src/components/redux/saga/GetData.ts
@@ -3,7 +3,7 @@ import { SET_ANIME_DATA } from "../actions/actionTypes";
 
 export function* getCardDatas(): Generator<any, void, any> {
   try {
-    let response = yield fetch("http://localhost:5000/animeCards");
+    let response = yield fetch("daily-animebe.vercel.app/animeCards");
     let reader = response.body.getReader();
     
     let result = yield reader.read();
