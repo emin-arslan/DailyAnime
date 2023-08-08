84736d9bac7a736dc339745e8341788bdfac3ca0
emin-arslan <eminarslan1864@gmail.com>
2023-08-08 12:51:32 +0300

    Update getCoverImage.js

--- backend/getCoverImage.js
+++ backend/getCoverImage.js
@@ -1,4 +1,5 @@
-const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
 const baseUrl = "https://myanimelist.net/search/all?q=";
 
