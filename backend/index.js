e789b1b24da6fe69e1986a807be5f856c219d959
emin-arslan <eminarslan1864@gmail.com>
2023-11-05 03:54:36 +0300

    Revert "Update index.js"
    This reverts commit 7300704a63f333ccbbec6eec986331d79ae37440.

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,7 @@ async function setAnimeDatas () {
 setAnimeDatas(); // İlk kez başlatmak için setAnimeDatas fonksiyonunu çağır
 
 app.get("/animeCards", async (req, resp) => {
-  
-  resp.status(200).json({ body: "await Anime.find({})" });
+  resp.status(200).json({ body: await Anime.find({}) });
 });
 
 app.listen(process.env.PORT || 5000);
